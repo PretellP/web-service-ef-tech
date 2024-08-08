260cf3196e45b4d66895900a33b5bc9b725a665b
PretellP <elw.danielpretell@gmail.com>
2024-08-08 18:38:36 -0500

    first commit

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$hostname = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$database = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
+$conexion = new mysqli($hostname, $username, $password, $database, $port);
+
+if ($conexion->connect_errno) {
+    echo "El sitio web está experimentado problemas";
+}
---
+++ login/validar_usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../conexion.php';
+
+// $usu_usuario = $_POST['usuario'];
+// $usu_password = $_POST['password'];
+
+$usu_usuario="admin";
+$usu_password="admin";
+
+$sentencia = $conexion->prepare("SELECT * FROM usuario WHERE usuario=? AND `password`=?");
+$sentencia->bind_param('ss', $usu_usuario, $usu_password);
+$sentencia->execute();
+
+$resultado = $sentencia->get_result();
+if ($fila = $resultado->fetch_assoc()) {
+    echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+}
+$sentencia->close();
+$conexion->close();
